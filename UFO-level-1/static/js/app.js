a283374a14fe62154c86b04676407358cea756d9
Erica <fisher1916@gmail.com>
2021-01-11 14:59:43 -0800

    cleaned node value code

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -3,16 +3,7 @@ var ufoData = data;
 console.log(ufoData);
 
 
-// Create a function to show only date
-// function dateTime(day) {
-//     return day.datetime;
-// }
-
-// var dateOnly = ufoData.map(dateTime);
-// console.log(dateOnly)
-
-
-// Create function to iterate through each sighting
+// Create function to iterate through each sighting, and add to table
 function showUfo(ufoSighting) {
 
     // Select table body
@@ -41,11 +32,7 @@ function showUfo(ufoSighting) {
 // Display all data
 showUfo(ufoData);
 
-// Select the form and button
-// var form = d3.select("#form");
-// var button = d3.select("#filter-btn");
-
-// Create listener
+// Create listeners for form enter and button
 d3.select("#filter-btn").on("click", handleSubmit);
 d3.select("#ufo-form").on("submit", handleSubmit);
 
@@ -54,20 +41,15 @@ function handleSubmit() {
     // Prevent the page from refreshing
     d3.event.preventDefault();
 
-    // // Set input value and get html node!! Why won't this work?
-    // var inputDate = d3.select("#datetime").node().value;
+    // Set input value and get html node, grab value of input
+    var inputValue = d3.select("#datetime").node().value;
 
-    // Select the input element and get the raw HTML node
-    var inputElement = d3.select("#datetime");
-
-    // Get the value property of the input element
-    var inputValue = inputElement.property("value");
-
+    // Filter data based on datetime input from user
     var ufoFilter = ufoData.filter((ufo) =>
     ufo.datetime === inputValue);
 
     console.log(inputValue);
-    
+
     // Display data on filtered date/time
     showUfo(ufoFilter);
 
